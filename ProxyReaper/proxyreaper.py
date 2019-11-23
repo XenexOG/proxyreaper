fee4696e96d83b4e86781c4ba4102747ebc6bb36
Xenex <54633237+XenexOG@users.noreply.github.com>
2019-11-23 23:50:21 +0100

    Version 1.1
    Features:
    +Config file
    +Prettier UI
    +Multi-threading for checking

--- ProxyReaper/proxyreaper.py
+++ ProxyReaper/proxyreaper.py
@@ -1,14 +1,12 @@
-from colorama import init, Fore
+from colorama import init
 import sys
 import os
-import requests
-from bs4 import BeautifulSoup
+
 import check
 import scrape
+from utils import *
 
 os.system('cls')
-
-test_url = 'google.com'
 
 init()
 
@@ -17,18 +15,18 @@ class proxyreaper():
         self.main()
 
     def main(self):
-        print(Fore.RED + """
+        print(red + """
     dBBBBBb dBBBBBb    dBBBBP`Bb  .BP dBP dBP dBBBBBb    dBBBP dBBBBBb   dBBBBBb  dBBBP dBBBBBb
        dB'     dBP   dBP.BP     .BP     dBP      dBP               BB       dB'            dBP
   dBBBP'  dBBBBK   dBP.BP    dBBK     dBP   dBBBBK'  dBBP     dBP BB   dBBBP' dBBP    dBBBBK
  dBP     dBP  BB  dBP.BP    dB'      dBP   dBP  BB  dBP      dBP  BB  dBP    dBP     dBP  BB
-dBP     dBP  dB' dBBBBP    dB' dBP  dBP   dBP  dB' dBBBBP   dBBBBBBB dBP    dBBBBP  dBP  dB'""")
-        print(Fore.BLUE + 'by Xenex\n\n\n')
-        print(Fore.RED + '[1] ' + Fore.WHITE + 'Scrape')
-        print(Fore.RED + '[2] ' + Fore.WHITE + 'Check')
-        print(Fore.RED + '[e] ' + Fore.WHITE + 'Exit')
+dBP     dBP  dB' dBBBBP    dB' dBP  dBP   dBP  dB' dBBBBP   dBBBBBBB dBP    dBBBBP  dBP  dB' v1.1""")
+        print(blue + 'by Xenex\n')
+        m = get('Main Menu\n' +\
+                red + '[' + blue + '1' + red + '] - ' + white + 'Scrape\n' +\
+                red + '[' + blue + '2' + red + '] - ' + white + 'Check\n' +\
+                red + '[' + blue + '3' + red + '] - ' + white + 'Exit\n')
 
-        m = input('>')
         if m == '1':
             os.system('cls')
             scrape.scrape()
@@ -40,7 +38,7 @@ dBP     dBP  dB' dBBBBP    dB' dBP  dBP   dBP  dB' dBBBBP   dBBBBBBB dBP    dBBB
             sys.exit(1)
         else:
             os.system('cls')
-            print(Fore.RED + 'ERROR: Input not recognised. Please retype and try again.')
+            error('Input not recognised. Please retype and try again.')
             self.main()
 
 if __name__ == '__main__':
--- ProxyReaper/scrape.py
+++ ProxyReaper/scrape.py
@@ -1,105 +1,117 @@
-from colorama import init, Fore
 import sys
 import os
 import requests
 from bs4 import BeautifulSoup
+import configparser
+
 import proxyreaper
+from utils import *
 
-def scrape():
-    type = ''
+class scrape:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.cfg')
 
-    print(Fore.RED + 'Select what type of proxy do you want to scrape\n\n\n')
-    print(Fore.RED + '[1] ' + Fore.WHITE + 'HTTP/S')
-    print(Fore.RED + '[2] ' + Fore.WHITE + 'SOCKS4')
-    print(Fore.RED + '[3] ' + Fore.WHITE + 'SOCKS5')
-    print(Fore.RED + '[b] ' + Fore.WHITE + 'Back')
+        self.proxies_type = ''
+        self.file_dir = config.get('scraper', 'file_dir')
+        self.threads = int(config.get('scraper', 'threads'))
 
-    m = input('>')
-    if m == '1':
-        type = 'http'
-    elif m == '2':
-        type = 'socks4'
-    elif m == '3':
-        type = 'socks5'
-    elif m == 'b':
+        self.proxies_type = ''
+
+        self.proxy_type()
+
+    def proxy_type(self):
+        m = get('Please specify the type of the proxies\n' +\
+                red + '[' + blue + '1' + red + '] - ' + white + 'HTTP/S\n' +\
+                red + '[' + blue + '2' + red + '] - ' + white + 'SOCKS4\n' +\
+                red + '[' + blue + '3' + red + '] - ' + white + 'SOCKS5\n')
+        
+        if m == '1':
+            self.proxies_type = 'http'
+            self.scrape_proxies()
+        elif m == '2':
+            self.proxies_type = 'socks4'
+            self.scrape_proxies()
+        elif m == '3':
+            self.proxies_type = 'socks5'
+            self.scrape_proxies()
+        else:
+            os.system('cls')
+            error('Input not recognised. Please retype and try again.')
+            self.proxy_type()
+
+    def scrape_proxies(self):
         os.system('cls')
-        proxyreaper.proxyreaper()
-    else:
-        os.system('cls')
-        print(Fore.RED + 'ERROR: Input not recognised. Please retype and try again.')
-        scrape()
 
-    os.system('cls')
-    print(Fore.RED + 'Started Scraping...')
+        action('Started scraping.')
 
-    # proxy-list.download
-    try:
-        print(Fore.BLUE + '\nScraping: ' + Fore.WHITE + 'proxy-list.downlod...')
-        r = requests.get('https://www.proxy-list.download/api/v1/get?type=' + type)
-        proxies = r.text
-        if type == 'http':
-            r2 = requests.get('https://www.proxy-list.download/api/v1/get?type=https')
-            proxies = proxies + r2.text
-        print(Fore.BLUE + 'Done.')
-    except Exception as e:
-        print(Fore.RED + 'ERROR: ' + Fore.WHITE + '{}'.format(e))
-        input(Fore.BLUE + 'Press any key to continue...')
-
-    # proxyscrape.com
-    try:
-        print(Fore.BLUE + '\nScraping: ' + Fore.WHITE + 'proxyscrape.com...')
-        r = requests.get('https://api.proxyscrape.com?request=displayproxies&proxytype=' + type)
-        proxies = proxies + r.text
-        print(Fore.BLUE + 'Done.')
-    except Exception as e:
-        print(Fore.RED + 'ERROR: ' + Fore.WHITE + '{}'.format(e))
-        input(Fore.BLUE + 'Press any key to continue...')
-
-    # proxy.rudnkh.me
-    if type == 'http':
+        # proxy-list.download
         try:
-            print(Fore.BLUE + '\nScraping: ' + Fore.WHITE + 'proxy.rudnkh.me...')
-            r = requests.get('https://proxy.rudnkh.me/txt')
+            action('Scraping proxy-list.downlod')
+            r = requests.get('https://www.proxy-list.download/api/v1/get?type=' + self.proxies_type)
+            proxies = r.text
+            if self.proxies_type == 'http':
+                r2 = requests.get('https://www.proxy-list.download/api/v1/get?type=https')
+                proxies = proxies + r2.text
+            action('Done.')
+        except Exception as e:
+            error('{}'.format(e))
+            get('Press any key to continue...')
+
+        # proxyscrape.com
+        try:
+            action('Scraping proxyscrape.com')
+            r = requests.get('https://api.proxyscrape.com?request=displayproxies&proxytype=' + self.proxies_type)
             proxies = proxies + r.text
-            print(Fore.BLUE + 'Done.')
+            action('Done.')
         except Exception as e:
-            print(Fore.RED + 'ERROR: ' + Fore.WHITE + '{}'.format(e))
-            input(Fore.BLUE + 'Press any key to continue...')
+            error('{}'.format(e))
+            get('Press any key to continue...')
 
-    # free-proxy-list.net
-    if type == 'http':
-        try:
-            print(Fore.BLUE + '\nScraping: ' + Fore.WHITE + 'free-proxy-list.net...')
-            r = requests.get('https://www.free-proxy-list.net/')
-            soup = BeautifulSoup(r.text, 'html.parser')
-            proxy_table_fpl = soup.select('table#proxylisttable tr')
-            for p in proxy_table_fpl:
-                info = p.find_all('td')
-                if len(info):
-                    proxy_fpl = ':'.join([info[0].text, info[1].text])
-            proxies = proxies + proxy_fpl
-        except Exception as e:
-            print(Fore.RED + 'ERROR: ' + Fore.WHITE + '{}'.format(e))
-            input(Fore.BLUE + 'Press any key to continue...')
+        # proxy.rudnkh.me
+        if type == 'http':
+            try:
+                action('Scraping proxy.rudnkh.me')
+                r = requests.get('https://proxy.rudnkh.me/txt')
+                proxies = proxies + r.text
+                action('Done.')
+            except Exception as e:
+                error('{}'.format(e))
+                get('Press any key to continue...')
 
-    # TODO: Add more proxy providers
+        # free-proxy-list.net
+        if type == 'http':
+            try:
+                action('Scraping free-proxy-list.net')
+                r = requests.get('https://www.free-proxy-list.net/')
+                soup = BeautifulSoup(r.text, 'html.parser')
+                proxy_table_fpl = soup.select('table#proxylisttable tr')
+                for p in proxy_table_fpl:
+                    info = p.find_all('td')
+                    if len(info):
+                        proxy_fpl = ':'.join([info[0].text, info[1].text])
+                proxies = proxies + proxy_fpl
+                action('Done.')
+            except Exception as e:
+                error('{}'.format(e))
+                get('Press any key to continue...')
 
-    print(Fore.RED + '\nRemoving duplicates...')
+        # TODO: Add more proxy providers
 
-    final_proxies = '\n'.join(set(proxies.split()))
-    lines = 0
+        action('Removing duplicates.')
 
-    with open('results_{}.txt'.format(type), 'w') as f:
-        f.write(final_proxies)
-        f.close()
+        final_proxies = '\n'.join(set(proxies.split()))
+        lines = 0
 
-    with open('results_{}.txt'.format(type), 'r') as f:
-        for line in f:
-            lines = lines + 1
-        f.close()
+        with open('results_{}.txt'.format(self.proxies_type), 'w') as f:
+            f.write(final_proxies)
+            f.close()
 
-    # TODO: Do some tidying
+        with open('results_{}.txt'.format(self.proxies_type), 'r') as f:
+            for line in f:
+                lines = lines + 1
+            f.close()
 
-    os.system('cls')
-    print(Fore.WHITE + 'Scraped ' + Fore.RED + '{} '.format(lines) + Fore.WHITE + 'proxies')
-    proxyreaper.proxyreaper()
+        os.system('cls')
+        action('Scraped ' + blue + '{} '.format(lines) + white + 'proxies.')
+        proxyreaper.proxyreaper()
